63e7c86fc14fdb9d6fe8e291e1d85995cd5c7af0
unknown <unknown@example.com>
2020-08-04 16:03:38 +0800

    Initial Commit

---
+++ miniprogram/pages/home/index.js
@@ -0,0 +1,66 @@
+// miniprogram/pages/home/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        list: [{ id: "1", progress: "0.5", title: "任务1", spend: "26", startTime: "2020-8-4" }, { id: "2", progress: "0.3", title: "任务3", spend: "43", startTime: "2020-8-4" }]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
